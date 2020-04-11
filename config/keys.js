5c12c135775455da966d456c3fb98da8ffa075c3
Abdiwahab Ahmed <abdiwahabahmed@Abdiwahabs-MacBook-Air.local>
2020-04-11 18:42:34 -0400

    added backend login and register for user

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+module.exports = {
+  mongoURI:
+    "mongodb+srv://ahmeda47:" +
+    process.env.MONGO_SECRET +
+    "@dbecentrix-fxu9c.mongodb.net/test?retryWrites=true&w=majority",
+};
